e8ff307e2ef6877c5f3178e89db6b7926c1c9f84
AntonRusev <102323013+AntonRusev@users.noreply.github.com>
2023-04-16 20:06:50 +0300

    implemented additional functionality

--- src/contexts/CalcContext.jsx
+++ src/contexts/CalcContext.jsx
@@ -6,97 +6,110 @@ export const CalcProvider = ({
     children
 }) => {
     const [displayValue, setDisplayValue] = useState('0');
+    const [firstNum, setFirstNum] = useState('0');
+    const [secondNum, setSecondNum] = useState('0');
+    const [operator, setOperator] = useState('');
     const [result, setResult] = useState('0');
     const [forCalculation, setForCalculation] = useState([]);
+
+    // useEffect(() => {
+    //     console.log(displayValue, firstNum, secondNum, operator);
+    // },[displayValue, firstNum, secondNum, operator]);
+
+    useEffect(() => {
+        calculate(firstNum, operator, secondNum);
+        setOperator('');
+    }, [secondNum]);
 
     const onBtnClick = (value) => {
         switch (value) {
             case '=':
-
+                setSecondNum(+displayValue);
                 break;
             case '+':
-
+                if (operator === '') {
+                    setOperator('+');
+                    setFirstNum(+displayValue);
+                    setDisplayValue('0');
+                } else {
+                    setSecondNum(+displayValue);
+                }
                 break;
             case '-':
-
+                if (operator === '') {
+                    setOperator('-');
+                    setFirstNum(+displayValue);
+                    setDisplayValue('0');
+                } else {
+                    setSecondNum(+displayValue);
+                }
                 break;
             case 'x':
-
+                if (operator === '') {
+                    setOperator('*');
+                    setFirstNum(+displayValue);
+                    setDisplayValue('0');
+                } else {
+                    setSecondNum(+displayValue);
+                }
                 break;
             case '/':
-
+                if (operator === '') {
+                    setOperator('/');
+                    setFirstNum(+displayValue);
+                    setDisplayValue('0');
+                } else {
+                    setSecondNum(+displayValue);
+                }
                 break;
             case '%':
-
-                break;
-            case '.':
-
+                if (operator === '') {
+                    setOperator('%');
+                    setFirstNum(+displayValue);
+                    setDisplayValue('0');
+                } else {
+                    setSecondNum(+displayValue);
+                }
                 break;
             case 'C':
-
+                setResult('0');
                 break;
             case 'CE':
-
+                setDisplayValue('0');
                 break;
 
             default:
+                if (displayValue.length > 0 && displayValue[0] === '.') {
+                    setDisplayValue(state => state.concat('0'+ state));
+                    setDisplayValue(state => state.slice(1));
+                }
                 setDisplayValue(state => state + value);
                 if (displayValue.length > 0 && displayValue[0] === '0') {
                     setDisplayValue(state => state.slice(1));
+                }
+                if (displayValue.length >= 9) {
+                    setDisplayValue(state => state.slice(0, 9));
                 }
                 break;
         }
     };
 
-    // const onBtnClick = (value) => {
-    //     if (
-    //         value === 'x'
-    //         || value === '/'
-    //         || value === '+'
-    //         || value === '-'
-    //     ) {
-    //         setForCalculation(state => [...state, displayValue, value]);
-    //         setDisplayValue('0')
-    //     } else if (value === '=') {
-    //         setForCalculation(state => [...state, displayValue]);
-    //         calculate();
-    //     } else {
-    //         setDisplayValue(state => state + value);
-    //         if (displayValue.length > 0 && displayValue[0] === '0') {
-    //             setDisplayValue(state => state.slice(1));
-    //         }
-    //     }
-    // };
-
-
-    const calculate = () => {
-        let finalResult = 0;
-        let currentNum = 0;
-        console.log(forCalculation.length)
-        for (let i = 0; i <= forCalculation.length; i++) {
-            if (i % 2 === 0) {
-                currentNum = +forCalculation[i];
-                console.log(forCalculation)
-                console.log(currentNum)
-            } else {
-                if (forCalculation[i] === '+') {
-                    finalResult += currentNum;
-                    console.log('+')
-                } else if (forCalculation[i] === '-') {
-                    finalResult -= currentNum;
-                    console.log('-')
-                } else if (forCalculation[i] === 'x') {
-                    finalResult *= currentNum;
-                    console.log('-')
-                } else if (forCalculation[i] === '/') {
-                    finalResult /= currentNum;
-                    console.log('/')
-                }
-            }
+    const calculate = (firstNum, operator, secondNum) => {
+        if (operator === '+') {
+            const result = +firstNum + +secondNum;
+            setDisplayValue(result);
+        } else if (operator === '-') {
+            const result = +firstNum - +secondNum;
+            setDisplayValue(result);
+        } else if (operator === '*') {
+            const result = +firstNum * +secondNum;
+            setDisplayValue(result);
+        } else if (operator === '/') {
+            const result = +firstNum / +secondNum;
+            setDisplayValue(result);
         }
-        // console.log(finalResult)
-        setResult(finalResult);
-        setDisplayValue(result);
+
+
     }
 
     const calcContextValue = {
